6fa22246c7dcc56326bc328a81e0d90ed62fc844
Liidia Ilves <liidia_ilves@hotmail.com>
2024-04-21 20:39:33 +0300

    Complete post page functionality

---
+++ client/src/pages/PostPage.jsx
@@ -0,0 +1,72 @@
+import { Button, Spinner } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+export default function PostPage() {
+  const { postSlug } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/post/getposts?slug=${postSlug}`);
+        const data = await res.json();
+
+        if (!res.ok) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        if (res.ok) {
+          setPost(data.posts[0]);
+          setLoading(false);
+          setError(false);
+        }
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchPost();
+  }, [postSlug]);
+
+  if (loading)
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <Spinner size="xl" />
+      </div>
+    );
+  return (
+    <main className="p-4 flex flex-col max-w-6xl mx-auto min-h-screen">
+      <h1 className="text-3xl mt-10 p-2 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
+        {post && post.title}
+      </h1>
+      <div className="flex justify-between p-2 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
+        <span className="self-center">
+          {post && new Date(post.createdAt).toLocaleDateString()}
+        </span>
+        <Link
+          to={`/search?category=${post && post.category}`}
+          className="self-center"
+        >
+          <Button color="gray" pill size="xs">
+            {post && post.category}
+          </Button>
+        </Link>
+      </div>
+      <img
+        src={post && post.image}
+        alt={post && post.title}
+        className="mt-10 p-2 max-h-[600px] w-full object-cover"
+      />
+      <div
+        //   "post-content" is css class styled in index.css
+        className="p-2 max-w-2xl mx-auto w-full post-content"
+        dangerouslySetInnerHTML={{ __html: post && post.content }}
+      ></div>
+    </main>
+  );
+}
