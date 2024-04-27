77ae6b1af83cbfe3ccd712980aacdf4ba115e108
Liidia Ilves <liidia_ilves@hotmail.com>
2024-04-27 23:28:10 +0300

    Add password confirmation to sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,22 +1,46 @@
 import { Link, useNavigate } from "react-router-dom";
-import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import {
+  Alert,
+  Button,
+  Label,
+  Spinner,
+  TextInput,
+  Modal,
+} from "flowbite-react";
 import { useState } from "react";
 import OAuth from "../components/OAuth";
 
 export default function SignUp() {
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
   const [errorMessage, setErrorMessage] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+    // const { id, value } = e.target;
+    // setFormData(prev => ({ ...prev, [id]: value }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!formData.username || !formData.email || !formData.password) {
+    if (
+      !formData.username ||
+      !formData.email ||
+      !formData.password ||
+      !formData.confirmPassword
+    ) {
       return setErrorMessage("Please fill out all fields.");
+    }
+
+    if (formData.password !== formData.confirmPassword) {
+      return setErrorMessage("Passwords do not match.");
     }
 
     try {
@@ -26,7 +50,11 @@ export default function SignUp() {
       const res = await fetch("/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
+        body: JSON.stringify({
+          username: formData.username,
+          email: formData.email,
+          password: formData.password,
+        }),
       });
       const data = await res.json();
 
@@ -36,7 +64,7 @@ export default function SignUp() {
       setLoading(false);
 
       if (res.ok) {
-        navigate("/sign-in");
+        setShowModal(true);
       }
     } catch (error) {
       setErrorMessage(error.message);
@@ -44,15 +72,20 @@ export default function SignUp() {
     }
   };
 
+  const handleModalClose = () => {
+    setShowModal(false);
+    navigate("/sign-in");
+  };
+
   return (
-    <div className="min-h-screnn mt-20">
+    <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         <div className="flex-1">
           <Link to="/" className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white mr-2">
-              Laiskliidu
+              Laiskliidu&apos;s
             </span>
-            Blogi
+            Blog
           </Link>
           <p className="text-sm mt-5">
             This is a blog created by Laiskliidu. You can sign up with your
@@ -80,11 +113,20 @@ export default function SignUp() {
               />
             </div>
             <div>
-              <Label value="Your password" />
+              <Label value="Your password once" />
               <TextInput
                 type="password"
                 placeholder="Password"
                 id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your password twice" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="confirmPassword"
                 onChange={handleChange}
               />
             </div>
@@ -115,6 +157,20 @@ export default function SignUp() {
               {errorMessage}
             </Alert>
           )}
+          <Modal show={showModal} onClose={handleModalClose} popup size="md">
+            {/* adds close button to modal automatically */}
+            <Modal.Header />
+            <Modal.Body>
+              <div className="text-center">
+                <h3 className="mb-4 text-lg text-gray-500 dark:text-gray-400">
+                  User registration succeeded!
+                </h3>
+                <h3 className="mb-4 text-lg text-gray-500 dark:text-gray-400">
+                  Now you can log in with your email and password.
+                </h3>
+              </div>
+            </Modal.Body>
+          </Modal>
         </div>
       </div>
     </div>
